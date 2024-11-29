5ef9a52d1930a32f5dc91584a6abe089ee148278
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-11-30 02:47:46 +0500

    Update admin.py
    -Добавили фичу для отправки уведомления всем

--- bot_backend/event_planner/admin.py
+++ bot_backend/event_planner/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event, Speaker, Session, SpeakerSession
+from .models import Event, Speaker, Session, SpeakerSession, User
+from .utils import get_schedule
+from telebot import TeleBot
 
 
 @admin.register(Event)
@@ -30,3 +32,19 @@ class SpeakerSessionAdmin(admin.ModelAdmin):
     list_display = ('speaker', 'topic', )
     search_fields = ('speaker__name', 'topic')
     ordering = ('session__event__date',)
+
+@admin.action(description="Рассылка о изменении в расписании")
+def send_massage_to_all_users(modeladmin, request, queryset):
+    bot = TeleBot("8066339717:AAFqYwMs5zx8va4mYBMgV8i9xFwXVK_4RKI")
+    users = User.objects.all()
+    massage_text = get_schedule()
+    for user in users:
+        bot.send_message(user.tg_id, massage_text)
+
+    modeladmin.message_user(
+        request, "Сообщения отправлены всем пользователям!")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    actions = [send_massage_to_all_users]
